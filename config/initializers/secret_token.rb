d410dc4daa89df7383bf46cd49146c0f20cb6f00
Jeff Horsch <jhorsch@gmail.com>
2014-01-28 10:58:08 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstHerokuApp::Application.config.secret_key_base = 'df525504d0a30aa6e618fe4ebd0cfa3aa0616b3bed5d814414d70021175074792c63651a142bc5ac0584036356ca1efed70d59e51f4836f44a51dd9bcc5d4bc9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FirstHerokuApp::Application.config.session_store :cookie_store, key: '_first-heroku-app_session'
